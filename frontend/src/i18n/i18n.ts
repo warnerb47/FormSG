c48a52256246c170186d53f4fd032e038083e5ef
warnerb47 <taphagaye2001.mg@gmail.com>
2025-05-31 17:09:19 +0000

    feat: set default language to french

--- frontend/src/i18n/i18n.ts
+++ frontend/src/i18n/i18n.ts
@@ -13,6 +13,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources: locales,
+    lng: Language.FRENCH,
     fallbackLng: Language.ENGLISH,
     debug: false,
     interpolation: {
